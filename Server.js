1d91e267b682988a713e98bd2dd0fba79a598f6a
Stoyann <stoyann.openclassrooms@gmail.com>
2023-06-08 20:28:32 +1100

    Initialisation de express

--- Server.js
+++ Server.js
@@ -1,1 +1,17 @@
-console.log('hello world')
+const express = require('express')
+const dotenv = require('dotenv')
+
+// LOAD ENV VARIABLES
+
+dotenv.config({ path: './config/config.env' })
+
+const app = express()
+
+const PORT = process.env.PORT || 5000
+
+app.listen(
+  PORT,
+  console.log(
+    `Le server a démarer sur le port ${process.env.PORT} en mode ${process.env.NODE_ENV}`,
+  ),
+)
